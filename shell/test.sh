0071de04baeb6248da7beef721a4f46a37df5a53
Chen Zhiwei <zhiweik@gmail.com>
2017-11-10 14:59:53 +0800

    update shell

--- shell/test.sh
+++ shell/test.sh
@@ -42,9 +42,51 @@ function app::has_ping() {
     which ping &>/dev/null
 }
 
+function app::string_operation() {
+    echo
+    # string in array
+    local str="are"
+    local arr=(how are you)
+    regx_arr=$(echo ${arr[@]} | sed 's/ /|/g')
+    if [[ $str =~ ($regx_arr) ]]; then
+        echo string is in an array
+    fi
+    if [[ $str =~ (how|are|you) ]]; then
+        echo string is in an array
+    fi
+    echo
+}
+
+function app::array_operation() {
+    local factor=(0:9 1:8 2:7 3:6 4:5)
+    for fact in ${factor[@]}; do
+        key=${fact%:*}
+        value=${fact##*:}
+        echo "key: $key, value: $value"
+    done
+    echo
+    for fact in ${factor[@]}; do
+        arr=(${fact//:/ })
+        key=${arr[0]}
+        value=${arr[1]}
+        echo "key: $key, value: $value"
+    done
+    echo
+    for fact in ${factor[@]}; do
+        arr=${fact//:/ }
+        set -- $arr
+        key=$1
+        value=$2
+        echo "key: $key, value: $value"
+    done
+}
+
+
 if app::has_ping; then
     echo there is ping
 fi
 
 app::component::stdout
 app::component::stderr
+app::array_operation
+app::string_operation
