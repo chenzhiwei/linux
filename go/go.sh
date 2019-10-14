6ac1c5fed6451b43b05209de8173bb275402ab97
Chen Zhiwei <zhiweik@gmail.com>
2019-10-14 12:24:58 +0800

    update go.sh

--- go/go.sh
+++ go/go.sh
@@ -3,7 +3,6 @@
 VERSION=${VERSION:-1.13.1}
 ARCH=$(uname -m|sed 's/x86_64/amd64/g')
 KERNAL=$(uname -s|tr '[:upper:]' '[:lower:]')
-
 URL=https://dl.google.com/go/go${VERSION}.${KERNAL}-${ARCH}.tar.gz
 
 if type curl &>/dev/null; then
@@ -12,13 +11,12 @@ else
     wget -O - $URL | sudo tar -xz -C /usr/local/
 fi
 
-chmod +x /usr/local/bin/dep
-
 cat <<'EOF' > $HOME/.golangrc
 # GOLANG
-[[ $PATH == */usr/local/go/bin* ]] || export PATH=$PATH:/usr/local/go/bin
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/dev/go
+[[ $PATH == *$GOROOT/bin* ]] || export PATH=$PATH:$GOROOT/bin
+[[ $PATH == *$GOPATH/bin* ]] || export PATH=$PATH:$GOPATH/bin
 EOF
 
 grep -wq '.golangrc' $HOME/.bashrc || echo '. $HOME/.golangrc' >> $HOME/.bashrc
