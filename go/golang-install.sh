f20c38696dfa423e993f027bccbc1f736dde5e3d
Chen Zhiwei <zhiweik@gmail.com>
2020-02-10 18:22:56 +0800

    update golang install path

---
+++ go/golang-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VERSION=${VERSION:-1.13.7}
+ARCH=$(uname -m|sed 's/x86_64/amd64/g')
+KERNAL=$(uname -s|tr '[:upper:]' '[:lower:]')
+URL=https://dl.google.com/go/go${VERSION}.${KERNAL}-${ARCH}.tar.gz
+
+GOINSTALL=$HOME/.golang/versions/$VERSION
+mkdir -p $GOINSTALL
+
+if type curl &>/dev/null; then
+    curl -kL $URL | tar -xz -C $GOINSTALL
+else
+    wget -O - $URL | tar -xz -C $GOINSTALL
+fi
+
+\rm -f $HOME/.golang/go
+ln -sf $GOINSTALL/go $HOME/.golang/go
+
+cat <<'EOF' > $HOME/.golang/rc
+# GOLANG
+export GOROOT=$HOME/.golang/go
+export GOPATH=$HOME/dev/go
+[[ $PATH == *$GOROOT/bin* ]] || export PATH=$GOROOT/bin:$PATH
+[[ $PATH == *$GOPATH/bin* ]] || export PATH=$GOPATH/bin:$PATH
+EOF
+
+grep -wq '.golang/rc' $HOME/.bashrc || echo '. $HOME/.golang/rc' >> $HOME/.bashrc
