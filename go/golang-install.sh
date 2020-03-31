6c73ca284e8456aec673ae76fb0d33128b1f997c
Chen Zhiwei <zhiweik@gmail.com>
2020-03-31 11:02:37 +0800

    update Rime dict

--- go/golang-install.sh
+++ go/golang-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=${VERSION:-1.13.7}
+VERSION=${VERSION:-1.14.1}
 ARCH=$(uname -m|sed 's/x86_64/amd64/g')
 KERNAL=$(uname -s|tr '[:upper:]' '[:lower:]')
 URL=https://dl.google.com/go/go${VERSION}.${KERNAL}-${ARCH}.tar.gz
